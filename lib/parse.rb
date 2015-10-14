1998ef9d9fcd35ce76f0590a701a85d90dd0a75f
sunghyukpark <sunghyukpark0408@gmail.com>
2015-10-14 13:39:46 -0500

    omit luhn check when instantiating card class

--- lib/parse.rb
+++ lib/parse.rb
@@ -11,12 +11,14 @@ File.open(file).each_line do |line|
   case line[0]
   when 'add'
     raise ArgumentError.new('Invalid inputs') unless line[1] && line[2] && line[3]
-    validator = LuhnValidator.new(line[2])
+
     card = Card.new(name: line[1].capitalize!,
                     card_num: line[2],
                     limit: line[3][1..-1].to_i)
     customer_accounts << card
+    p customer_accounts
   when 'charge'
+
   when 'credit'
 
   end
